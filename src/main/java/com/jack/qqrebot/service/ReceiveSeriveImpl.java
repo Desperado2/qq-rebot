2b549b8d27765a85e5205a8c9f256a3d26e1f646
mujj <mujj@jackmu.cn>
2019-03-19 14:18:21 +0800

    排行榜支持排序

--- src/main/java/com/jack/qqrebot/service/ReceiveSeriveImpl.java
+++ src/main/java/com/jack/qqrebot/service/ReceiveSeriveImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.net.URLEncoder;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,24 +39,25 @@ public class ReceiveSeriveImpl implements ReceiveServiceI {
 
     private String getPhb(){
         String text = "";
-        String text1 = "";
+        StringBuilder text1 = new StringBuilder();
         try {
             File file = new File("C:\\CQPro\\data\\phb\\score.txt");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"utf-8"));
 
             while ((text=bufferedReader.readLine()) != null){
-                text1+=text+"\n";
+                text1.append(text).append("\n");
             }
             bufferedReader.close();
+            sort(text1);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return text1;
+        return text1.toString();
     }
 
     private String addScore(String name,int score,String type){
 
-        String text1 = "";
+        StringBuilder sb = new StringBuilder();
         try {
             File file = new File("C:\\CQPro\\data\\phb\\score.txt");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"utf-8"));
@@ -70,20 +72,65 @@ public class ReceiveSeriveImpl implements ReceiveServiceI {
                     }else{
                         text = name+"       "+(Integer.parseInt(m.replaceAll("").trim())-score)+"分";
                     }
-
                 }
-                text1 += text+"\n";
-
+               sb.append(text).append("\n");
             }
            bufferedReader.close();
-
+            sort(sb);
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"utf-8"));
-            bufferedWriter.write(text1);
+            bufferedWriter.write(sb.toString());
             bufferedWriter.flush();
             bufferedWriter.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return text1;
+        return sb.toString();
+    }
+
+    private void sort(StringBuilder text){
+        String[] strings = text.toString().split("\n");
+        TreeMap<Integer,String> treeMap = new TreeMap<>((o1, o2) -> -o1.compareTo(o2));
+        int index=0;
+        int index1= 0;
+        for (int i=0;i<strings.length; i++){
+            if(isContains(strings[i])){
+                index1 = index;
+                String regEx="[^0-9]";
+                Pattern p = Pattern.compile(regEx);
+                Matcher m = p.matcher(strings[i]);
+                int key =Integer.parseInt(m.replaceAll("").trim());
+                if(treeMap.containsKey(key))
+                    key++;
+                treeMap.put(key,strings[i]);
+            }else{
+                index++;
+            }
+        }
+
+        for (Integer key : treeMap.keySet()){
+            strings[index1] = treeMap.get(key);
+            index1++;
+        }
+        text.delete(0,text.length());
+        for (int i=0;i<strings.length; i++){
+            text.append(strings[i]).append("\n");
+        }
+    }
+
+    private boolean isContains(String text){
+        List<String> list = new ArrayList<>();
+        list.add("张亚泰");
+        list.add("黄博");
+        list.add("郝红剑");
+        list.add("胡军");
+        list.add("辛季凯");
+        list.add("陈宗明");
+
+        for (String name:list){
+            if (text.contains(name)){
+                return true;
+            }
+        }
+        return false;
     }
 }
