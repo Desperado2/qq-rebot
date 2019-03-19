99634a50df73d1fbfd944f83382cca6f19973635
mujj <mujj@jackmu.cn>
2019-03-19 18:14:09 +0800

    重构

--- src/test/java/com/jack/qqrebot/QqRebotApplicationTests.java
+++ src/test/java/com/jack/qqrebot/QqRebotApplicationTests.java
@@ -1,6 +1,6 @@
 package com.jack.qqrebot;
 
-import com.jack.qqrebot.service.ReceiveServiceI;
+import com.jack.qqrebot.service.ranking.RankingService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +13,9 @@ import java.io.UnsupportedEncodingException;
 @SpringBootTest
 public class QqRebotApplicationTests {
 
-	@Autowired
-	private ReceiveServiceI receiveService;
 	@Test
 	public void contextLoads() {
-		try {
-			receiveService.phb(112,"排行榜|hb|+|dd");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
+
 	}
 
 }
