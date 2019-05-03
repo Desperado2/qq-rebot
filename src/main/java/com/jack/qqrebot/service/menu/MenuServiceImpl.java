2aaaf7391db270cb909b3371f3c2a2b8f611fe6a
mujj <mujj@jackmu.cn>
2019-05-03 16:03:49 +0800

    update

--- src/main/java/com/jack/qqrebot/service/menu/MenuServiceImpl.java
+++ src/main/java/com/jack/qqrebot/service/menu/MenuServiceImpl.java
@@ -17,7 +17,7 @@ public class MenuServiceImpl implements MenuService {
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer.append("功能列表\n\n");
         String[] array = new String[]{"诗","新闻","笑话/段子","美图","音乐","天气","微博/热搜","老黄历","星座","毒鸡汤","干货",
-                "干货日报类型","资源","v2ex","leetcode","土味情话","二次元图片","menhera酱"};
+                "干货日报类型","资源","v2ex","leetcode","土味情话","二次元图片","menhera酱","snh"};
         for (int i=0;i<array.length;i++){
             stringBuffer.append((i+1)).append(". ").append(array[i]).append("\n");
         }
