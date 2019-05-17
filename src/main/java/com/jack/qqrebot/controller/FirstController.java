a1a13b0e9148e0b5bcb0a8558044bec9448fa2f6
mujj <mujj@jackmu.cn>
2019-05-17 12:49:58 +0800

    add 访问次数控制

--- src/main/java/com/jack/qqrebot/controller/FirstController.java
+++ src/main/java/com/jack/qqrebot/controller/FirstController.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.jack.qqrebot.service.SendServiceI;
 import com.jack.qqrebot.utils.CQUtils;
 import com.jack.qqrebot.utils.SendMsgUtils;
+import com.jack.qqrebot.utils.UpdateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,21 +29,28 @@ public class FirstController {
 
     @RequestMapping(value = "/get", method = RequestMethod.POST)
     public void get(@RequestBody String message) throws UnsupportedEncodingException {
-
         JSONObject jsonObject = JSON.parseObject(message);
         String messageType = jsonObject.getString("message_type");
         String postType = jsonObject.getString("post_type");
         if(postType.equals("notice")){
-           sendService.dealNotice(message);
-        }else if("group".equals(messageType)){
+             sendService.dealNotice(message);
+        }else if("group".equals(messageType)) {
             sendService.dealGroupMsg(message);
         }
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.GET)
-    public void github() {
+    public void update() {
         List<Integer> groupList = CQUtils.getGroupList();
         String messages = "通知\n机器人将于10秒后开始升级，升级期间无法使用，升级完成后会通知，谢谢";
         groupList.forEach(groupId->SendMsgUtils.sendGroupMsg(groupId, messages));
     }
+
+    @RequestMapping(value = "/updateSuccess", method = RequestMethod.GET)
+    public void updateSuccess() {
+        List<Integer> groupList = CQUtils.getGroupList();
+        String messages = "通知\n机器人升级完成,\n"+UpdateUtils.getUpdate();
+        groupList.forEach(groupId -> SendMsgUtils.sendGroupMsg(groupId, messages));
+    }
+
 }
